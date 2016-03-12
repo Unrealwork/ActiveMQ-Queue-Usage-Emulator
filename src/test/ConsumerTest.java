ba44ddfe3ad0574d67b56005303728e9ae1995ef
Igor Shmagrinskiy <unrealowork@gmail.com>
2016-03-13 01:17:51 +0300

    codestyle edited

---
+++ src/test/ConsumerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created by shmagrinsky on 13.03.16.
+ */
+public class ConsumerTest {
+
+    @Test
+    public void testGetQueueSize() throws Exception {
+
+    }
+
+    @Test
+    public void testReceiveMessage() throws Exception {
+
+    }
+}
